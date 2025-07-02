309a03f5cc7c42fdb370321090d5d839da5fddd5
Vincent-cmd1 <faivre.vinz@gmail.com>
2025-07-02 14:47:12 +0200

    Modification du projet

---
+++ Bagarre_au_bar/Classes/Ivrogne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Ivrogne
+{
+	public Ivrogne()
+	{
+
+    private string nom  // Le nom de l'ivrogne.
+    private int pointsDeVie  // Les points de vie(PV) de l'ivrogne.
+    private int nbDesAttaque  // Le nombre de dés d’attaque.
+
+    }
+}
