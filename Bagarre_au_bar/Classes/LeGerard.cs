d9531e6d75f77b9fa2035af7125472d613df899f
Vincent-cmd1 <faivre.vinz@gmail.com>
2025-07-03 09:11:24 +0200

    Modification

--- Bagarre_au_bar/Classes/LeGerard.cs
+++ Bagarre_au_bar/Classes/LeGerard.cs
@@ -3,19 +3,28 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class LeGerard
+public class LeGerard : Ivrogne
 {
 	public LeGerard()
 	{
     
     // -- PROPRIETES --
 
-
+    
 
 
 
 
     // -- CONSTRUCTEUR --
+    
+    public LeGerard(string nom, int pointsDeVie) : base(nom, pointDeVie)
+    {
+        Nom = nom;
+    }
+
+
+
+
 
 
 
