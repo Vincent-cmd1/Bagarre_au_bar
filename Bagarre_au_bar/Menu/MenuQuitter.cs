5f45d4aba508350a5f3bc68b37bce5dfd6b583ac
Vincent <faivre.vinz@gmail.com>
2025-07-04 11:12:06 +0200

    Feat : Menu
    Ajout d'une partie du Menu et de la classe Renaud

---
+++ Bagarre_au_bar/Menu/MenuQuitter.cs
@@ -0,0 +1,18 @@
+﻿public class MenuQuitter
+{
+    public static void AfficherQuitter()
+    {
+        Console.Clear();
+        Console.WriteLine("╔═══════════════════════════════════════════════════════════╗");
+        Console.WriteLine("║                                                           ║");
+        Console.WriteLine("║ Fin de soirée... les verres sont cassés, les dents aussi. ║");
+        Console.WriteLine("║ T’as survécu à l’odeur de sueur, au Ricard tiède,         ║");
+        Console.WriteLine("║ et aux punchlines de Gérard. Va te coucher, champion...   ║");
+        Console.WriteLine("║ demain, va falloir survivre à la gueule de bois...        ║");
+        Console.WriteLine("║                                                           ║");
+        Console.WriteLine("║═══════════════════════════════════════════════════════════║");
+        Console.WriteLine("║ Crédits : Cédric L. && Vincent F.,                        ║");
+        Console.WriteLine("║           sous la bienveillance d'Alexandre L.            ║");
+        Console.WriteLine("╚═══════════════════════════════════════════════════════════╝");
+    }
+}
---
+++ Bagarre_au_bar/Menu/MenuStructure.cs
@@ -0,0 +1,28 @@
+﻿public class MenuStructure
+{
+    public static void AfficherMenu()
+    {
+        bool ProgrammeLance = true;
+        while (ProgrammeLance)
+        {
+            MenuAffichage.AfficherMenu();
+            string choixutilisateur = Console.ReadLine();
+
+            switch (choixutilisateur)
+            {
+                case "0":
+                    ProgrammeLance = false;
+                    MenuQuitter.AfficherQuitter();
+                    break;
+
+                case "6":
+                    Regles.AfficherRegles();
+                    Console.ReadKey();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
