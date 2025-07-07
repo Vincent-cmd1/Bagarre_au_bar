9694195b909ee6705300f45767b4bf4703b6c40a
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-07 16:16:02 +0200

    refacto: changed all lists to one "CombattantsManager"

---
+++ Bagarre_au_bar/Modals/CombattantsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class CombattantsManager
+{
+    public static List<Ivrogne> ObtenirTousLesCombattants()
+    {
+        return new List<Ivrogne>
+        {
+            new LeRenaudTjVivant(),
+            new LeRenaudTeigne(),
+            new GerardMince(),
+            new GrosGerard(),
+            new JohnnyDeFeu(),
+            new JohnnyCadillac()
+        };
+    }
+}
--- Bagarre_au_bar/obj/Debug/net8.0/Bagarre_au_bar.AssemblyInfo.cs
+++ Bagarre_au_bar/obj/Debug/net8.0/Bagarre_au_bar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bagarre_au_bar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c4ce394ed5188c4bbc22242fe0957b87accd41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e63d6792c5c18933f31983f73f907d49838de7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bagarre_au_bar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bagarre_au_bar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
